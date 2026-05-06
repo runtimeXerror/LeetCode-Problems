6e80c64121fe53c9438b5a6a1a689d8939756d6a
Vishal Kumar <154529583+runtimeXerror@users.noreply.github.com>
2026-05-07 00:02:02 +0530

    Time: 18 ms (26.54%), Space: 56.8 MB (69.53%) - LeetHub

---
+++ 1861-rotating-the-box/1861-rotating-the-box.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& boxGrid){
+        int m = boxGrid.size();
+        int n = boxGrid[0].size();
+
+        // STEP 1: Apply gravity (move stones to right)
+        for(int i = 0; i < m; i++){
+            int empty = n - 1;
+            for(int j = n - 1; j >= 0; j--){
+                if(boxGrid[i][j] == '*'){
+                    empty = j - 1;
+                }
+                else if(boxGrid[i][j] == '#'){
+                    swap(boxGrid[i][j], boxGrid[i][empty]);
+                    empty--;
+                }
+            }
+        }
+
+        // STEP 2: Rotate matrix clockwise
+        vector<vector<char>> ans(n, vector<char>(m));
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                ans[j][m - 1 - i] = boxGrid[i][j];
+            }
+        }
+        return ans;
+    }
+};
