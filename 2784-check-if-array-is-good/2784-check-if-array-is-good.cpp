a4938c98a9edadedd52f26adc664cd0a424ab8c8
Vishal Kumar <154529583+runtimeXerror@users.noreply.github.com>
2026-05-14 23:20:43 +0530

    Time: 0 ms (100%), Space: 26.5 MB (73.39%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.cpp
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.cpp
@@ -1,28 +1,13 @@
 class Solution {
 public:
-    bool isGood(vector<int>& nums) {
-        int n = *max_element(nums.begin(), nums.end());
-
-        // Step 1: check length
-        // if (nums.size() != n + 1) return false;
-
-        // Step 2: frequency count
-        vector<int> freq(n + 1, 0);
-
-        for (int num : nums) {
-            if (num > n) return false;
-            freq[num]++;
+    bool isGood(vector<int>& nums){
+        sort(nums.begin(), nums.end());
+        int n = nums.size()-1;
+        for(int i = 0; i < n; ++i){
+            if(nums[i] != i + 1){
+                return false;
+            }
         }
-
-        // Step 3: validate
-        for (int i = 1; i < n; i++) {
-            if (freq[i] != 1) return false;
-        }
-
-        // n should appear twice
-        if (freq[n] != 2) return false;
-
-        return true;
-    
+        return nums[n] == n;
     }
 };
