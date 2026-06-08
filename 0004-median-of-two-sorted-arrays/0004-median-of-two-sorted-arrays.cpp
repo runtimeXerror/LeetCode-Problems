5584a4a15bc113fd682c4952b62e69f0538a7593
Vishal Kumar <154529583+runtimeXerror@users.noreply.github.com>
2026-06-08 22:39:08 +0530

    Time: 0 ms (100%), Space: 96.1 MB (8.89%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.cpp
@@ -1,60 +1,25 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        // Ensure nums1 is the smaller array
-    if (nums1.size() > nums2.size()) {
-        std::swap(nums1, nums2);
-    }
+        vector<int> merged;
+        int i = 0, j = 0;
 
-    int m = nums1.size();
-    int n = nums2.size();
-    int imin = 0, imax = m, halfLen = (m + n + 1) / 2;
-
-    while (imin <= imax) {
-        int i = (imin + imax) / 2; // partition nums1
-        int j = halfLen - i;       // partition nums2
-
-        // If i is too small, we need to increase it
-        if (i < m && nums2[j - 1] > nums1[i]) {
-            imin = i + 1; // i is too small, must increase it
+        while (i < nums1.size() && j < nums2.size())  {
+            if (nums1[i] <= nums2[j])
+                merged.push_back(nums1[i++]);
+            else
+                merged.push_back(nums2[j++]);
         }
-        // If i is too big, we need to decrease it
-        else if (i > 0 && nums1[i - 1] > nums2[j]) {
-            imax = i - 1; // i is too big, must decrease it
-        }
-        // i is perfect
-        else {
-            // Get max of left side
-            int maxLeft = 0;
-            if (i == 0) {
-                maxLeft = nums2[j - 1]; // nums1 is empty
-            } else if (j == 0) {
-                maxLeft = nums1[i - 1]; // nums2 is empty
-            } else {
-                maxLeft = std::max(nums1[i - 1], nums2[j - 1]);
-            }
 
-            // If the total number of elements is odd, return the max of left side
-            if ((m + n) % 2 == 1) {
-                return maxLeft;
-            }
+        while (i < nums1.size()) merged.push_back(nums1[i++]);
+        while (j < nums2.size()) merged.push_back(nums2[j++]);
 
-            // Get min of right side
-            int minRight = 0;
-            if (i == m) {
-                minRight = nums2[j]; // nums1 is empty
-            } else if (j == n) {
-                minRight = nums1[i]; // nums2 is empty
-            } else {
-                minRight = std::min(nums1[i], nums2[j]);
-            }
+        int n = merged.size();
+        int mid = n / 2;
 
-            // Return the average of max of left side and min of right side
-            return (maxLeft + minRight) / 2.0;
-        }
-    }
-
-    // If we get here, there is an error
-    throw std::invalid_argument("Input arrays are not sorted.");
+        if (n % 2 == 1)
+            return (double)merged[mid];
+        else
+            return (merged[mid-1] + merged[mid]) / 2.0;
     }
 };
