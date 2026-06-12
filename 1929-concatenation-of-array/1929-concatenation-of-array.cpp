195d831791259b39e712208e7fd75c14bbd15cd3
Vishal Kumar <154529583+runtimeXerror@users.noreply.github.com>
2026-06-13 04:40:03 +0530

    Time: 0 ms (100%), Space: 16.8 MB (58.49%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.cpp
@@ -0,0 +1,8 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int> ans=nums;
+        ans.insert(ans.end(), ans.begin(), ans.end());
+        return ans;
+    }
+};
