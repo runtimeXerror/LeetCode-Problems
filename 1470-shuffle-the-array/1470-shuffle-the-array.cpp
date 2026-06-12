310988c9cd6285f19fa6f552032716ea28655943
Vishal Kumar <154529583+runtimeXerror@users.noreply.github.com>
2026-06-13 05:21:43 +0530

    Time: 8 ms (16.89%), Space: 13.3 MB (79.66%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.cpp
+++ 1470-shuffle-the-array/1470-shuffle-the-array.cpp
@@ -3,11 +3,10 @@ public:
     vector<int> shuffle(vector<int>& nums, int n) {
         vector<int> ans;
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++){
             ans.push_back(nums[i]);
             ans.push_back(nums[i + n]);
         }
-
         return ans;
     }
 };
